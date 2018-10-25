94aaaa9c55b36fa47bd6d30d502f6625be6697e5
Кирилл <kirill@iMac-Filatov.local>
2018-10-25 18:17:24 +0300

    Initial Commit

---
+++ agro24/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  agro24
+//
+//  Created by Кирилл on 25/10/2018.
+//  Copyright © 2018 a24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
